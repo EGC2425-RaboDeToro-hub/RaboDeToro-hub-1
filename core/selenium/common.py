e5f03beeb5feb61d983f6557399984e103808021
Paula Maria Suarez Linares <a.pausualin@gmail.com>
2024-11-13 23:17:07 +0100

    fix: Corregir error common

--- core/selenium/common.py
+++ core/selenium/common.py
@@ -3,7 +3,7 @@ from selenium.webdriver.chrome.service import Service
 
 
 def initialize_driver():
-    service = Service('/home/virginia/.wdm/drivers/chromedriver/linux64/130.0.6723.69/chromedriver-linux64/chromedriver'
+    service = Service('/home/pausualin/.wdm/drivers/chromedriver/linux64/130.0.6723.69/chromedriver-linux64/chromedriver'
                       )
     options = webdriver.ChromeOptions()
     driver = webdriver.Chrome(service=service, options=options)
