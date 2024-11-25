62f604fe8ec18d34073d8b59cc27a815753270ed
paulasanchezg <pausangom1@alum.us.es>
2024-11-25 19:28:37 +0100

    test: Añadir test integracion
    Se han añadido los test de integracion de filtro avanzado.
    
    Refs #20

--- app/modules/dataset/tests/test_integration.py
+++ app/modules/dataset/tests/test_integration.py
@@ -21,3 +21,21 @@ def test_download_all_datasets(test_client):
     assert response.headers["Content-Type"] == "application/zip"
 
     logout(test_client)
+    
+
+def test_filter_datasets_by_features(test_client):
+    # Test filtering by features
+    response = test_client.get("/api/v1/datasets/filtered?min_features=5&max_features=20")
+    assert response.status_code == 200
+
+    data = response.json  # Aquí se accede directamente sin llamarlo como función
+    assert all(5 <= dataset["feature_count"] <= 20 for dataset in data), "Datasets not filtered correctly by features."
+
+
+def test_filter_datasets_by_products(test_client):
+    # Test filtering by products
+    response = test_client.get("/api/v1/datasets/filtered?min_products=30&max_products=60")
+    assert response.status_code == 200
+
+    data = response.json  # Aquí se accede directamente sin llamarlo como función
+    assert all(30 <= dataset["product_count"] <= 60 for dataset in data), "Datasets not filtered correctly by products."
