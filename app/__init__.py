e169859c81e06f3660365ba444be13954d2437ed
JuanAntonioMorenoMoguel <juamormog@gmail.com>
2024-12-07 17:47:35 +0100

    feat: Script y readme
    Script de incializacion y caracteristicas añadidas al readme
    
    Refs#69

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,5 @@
 import os
+import shutil
 
 from flask import Flask
 
@@ -25,16 +26,40 @@ session = Session()
 
 
 def clear_logs_and_sessions():
-    if os.path.exists("logs"):
-        for file in os.listdir("logs"):
-            os.remove(os.path.join("logs", file))
-    if os.path.exists("flask_session"):
-        for file in os.listdir("flask_session"):
-            os.remove(os.path.join("flask_session", file))
+    # Eliminar archivos y enlaces simbólicos en la carpeta logs
+    logs_path = "logs"
+    if os.path.exists(logs_path):
+        for file in os.listdir(logs_path):
+            file_path = os.path.join(logs_path, file)
+            try:
+                if os.path.isfile(file_path) or os.path.islink(file_path):
+                    os.unlink(file_path)
+                elif os.path.isdir(file_path):
+                    shutil.rmtree(file_path)
+                print(f"Deleted log file: {file_path}")
+            except Exception as e:
+                print(f"Failed to delete {file_path}. Reason: {e}")
+
+    # Eliminar archivos y enlaces simbólicos en la carpeta flask_session
+    session_path = "flask_session"
+    if os.path.exists(session_path):
+        for file in os.listdir(session_path):
+            file_path = os.path.join(session_path, file)
+            try:
+                if os.path.isfile(file_path) or os.path.islink(file_path):
+                    os.unlink(file_path)
+                elif os.path.isdir(file_path):
+                    shutil.rmtree(file_path)
+                print(f"Deleted session file: {file_path}")
+            except Exception as e:
+                print(f"Failed to delete {file_path}. Reason: {e}")
 
 
 def create_app(config_name="development"):
     app = Flask(__name__)
+
+    # Llama a la función para limpiar logs y sesiones antes de iniciar la aplicación
+    clear_logs_and_sessions()
 
     # Load configuration according to environment
     config_manager = ConfigManager(app)
@@ -52,7 +77,7 @@ def create_app(config_name="development"):
 
     # Deletes all logs and flask_session files
     clear_logs_and_sessions()
-    
+
     # Register modules
     module_manager = ModuleManager(app)
     module_manager.register_modules()
