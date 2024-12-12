ff7e6e7a94e3503dfe8212e7a4308a923ad05989
JuanAntonioMorenoMoguel <juamormog@gmail.com>
2024-12-12 16:25:53 +0100

    fix: 55%cov
    Ya tenemos 55%
    
    Refs#72

--- app/modules/fakenodo/tests/test_unit.py
+++ app/modules/fakenodo/tests/test_unit.py
@@ -32,6 +32,9 @@ def fakenodo_service():
 def test_test_connection(fakenodo_service):
     assert fakenodo_service.test_connection() is True
 
+def test_generate_doi(fakenodo_service):
+    doi = fakenodo_service._generate_doi(123)
+    assert doi == "10.1234/dataset123"
 
 def test_create_new_deposition(fakenodo_service, mock_dataset):
     mock_deposition = MagicMock()
