7bcf4f717c8d7a4c65c8f4df0b4e4c665b626bb8
virmesper <virmesper@alum.us.es>
2024-12-12 15:51:14 +0100

    fix:Arreglar chrome
    Se ha arreglado selenium.
    
    Refs #null

--- core/selenium/common.py
+++ core/selenium/common.py
@@ -1,11 +1,17 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.options import Options
 
 
 def initialize_driver():
     service = Service(ChromeDriverManager().install())
-    options = webdriver.ChromeOptions()
+    options = Options()
+    options.add_argument("--headless")  # Ejecutar en modo sin interfaz gráfica
+    options.add_argument("--no-sandbox")  # Requerido en entornos CI/CD
+    options.add_argument("--disable-dev-shm-usage")  # Usa /tmp en lugar de /dev/shm
+    options.add_argument("--disable-gpu")  # Desactiva el uso de la GPU
+    options.add_argument("--window-size=1920,1080")  # Tamaño de ventana para evitar problemas de visualización
     driver = webdriver.Chrome(service=service, options=options)
     return driver
 
