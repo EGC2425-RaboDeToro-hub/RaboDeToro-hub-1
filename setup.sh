1bccf7bdfa8f28dbc5cd29e01bfed4efe36ea36a
juan_antonio_moreno_moguel <juamormog@gmail.com>
2024-12-12 12:01:07 +0100

    Update setup.sh
    Ya no crea migraciones

--- setup.sh
+++ setup.sh
@@ -88,8 +88,6 @@ pip install -r requirements.txt
 pip install -e ./
 
 # Iniciar la aplicacion
-rosemary db:reset -y
-rosemary db:migrate
 rosemary db:seed
 flask db upgrade
 flask run --host=0.0.0.0 --port=5000
