38df0114728a79b0177497f0762fff2b8191a0d4
Jose Morgado Prudencio <josmorpru@alum.us.es>
2024-12-15 22:25:11 +0100

    feat: Arreglar sender

--- app/modules/email/services.py
+++ app/modules/email/services.py
@@ -14,14 +14,14 @@ class EmailService(BaseService):
     def init_app(self, app):
         # Carga las variables de entorno desde el archivo .env
         load_dotenv()
-
-        app.config["MAIL_SERVER"] = os.getenv("MAIL_SERVER", "smtp.office365.com")
+        app.config["MAIL_SERVER"] = os.getenv("MAIL_SERVER", "smtp.gmail.com")
         app.config["MAIL_PORT"] = int(os.getenv("MAIL_PORT", "587"))
         app.config["MAIL_USE_TLS"] = os.getenv("MAIL_USE_TLS", "True") == "True"
         app.config["MAIL_USE_SSL"] = os.getenv("MAIL_USE_SSL", "False") == "True"
         app.config["MAIL_USERNAME"] = os.getenv("MAIL_USERNAME")
         app.config["MAIL_PASSWORD"] = os.getenv("MAIL_PASSWORD")
         app.config["MAIL_DEFAULT_SENDER"] = os.getenv("MAIL_DEFAULT_SENDER")
+
 
         self.mail = Mail(app)
         self.sender = app.config["MAIL_USERNAME"]
