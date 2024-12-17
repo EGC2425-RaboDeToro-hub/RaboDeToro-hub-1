029401567961ed45bcc43c9541310b94c9e7f574
marioreyesapresa <marreyapr@alum.us.es>
2024-12-17 12:15:23 +0100

    feat: Añadir test upload
    Se ha añadido el test upload dataset.
    
    Refs #null

--- app/modules/dataset/tests/test_selenium.py
+++ app/modules/dataset/tests/test_selenium.py
@@ -6,6 +6,7 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
+import os
 
 
 class TestDatasets:
@@ -34,6 +35,43 @@ class TestDatasets:
         except Exception:
             amount_datasets = 0
         return amount_datasets
+
+    def test_upload_dataset(self):
+        host = "http://localhost:5000"
+        # Abrir la página de login
+        self.driver.get(f"{host}/login")
+        self.wait_for_page_to_load()
+        # Ingresar credenciales y enviar el formulario
+        self.driver.find_element(By.NAME, "email").send_keys("user1@example.com")
+        self.driver.find_element(By.NAME, "password").send_keys("1234", Keys.RETURN)
+        self.wait_for_page_to_load()
+        # Contar datasets iniciales
+        initial_datasets = self.count_datasets(host)
+        # Abrir la página de subir dataset
+        self.driver.get(f"{host}/dataset/upload")
+        self.wait_for_page_to_load()
+        # Completar el formulario de subida
+        self.driver.find_element(By.NAME, "title").send_keys("Title")
+        self.driver.find_element(By.NAME, "desc").send_keys("Description")
+        self.driver.find_element(By.NAME, "tags").send_keys("tag1,tag2")
+        # Añadir autores
+        self.driver.find_element(By.ID, "add_author").send_keys(Keys.RETURN)
+        self.wait_for_page_to_load()
+        self.driver.find_element(By.NAME, "authors-0-name").send_keys("Author0")
+        self.driver.find_element(By.NAME, "authors-0-affiliation").send_keys("Club0")
+        # Subir archivos UVL
+        file1_path = os.path.abspath("app/modules/dataset/uvl_examples/file1.uvl")
+        self.driver.find_element(By.CLASS_NAME, "dz-hidden-input").send_keys(file1_path)
+        self.wait_for_page_to_load()
+        # Aceptar términos y enviar
+        self.driver.find_element(By.ID, "agreeCheckbox").send_keys(Keys.SPACE)
+        self.driver.find_element(By.ID, "upload_button").send_keys(Keys.RETURN)
+        self.wait_for_page_to_load()
+        time.sleep(2)
+        # Validar resultados
+        assert self.driver.current_url == f"{host}/dataset/list", "Error al regresar al listado"
+        final_datasets = self.count_datasets(host)
+        assert final_datasets == initial_datasets + 1, "El dataset no fue añadido correctamente"
 
     def test_download_all_datasets(self):
         host = "http://localhost:5000"
